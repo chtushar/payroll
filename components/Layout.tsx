dc8d68b7ac81451b7d7d61424fbc6e7e3b1bf5f1
Tushar Choudhari <tushar.choudhari@tickertape.in>
2022-08-26 13:05:34 +0530

    :lipstick: Add layout

---
+++ components/Layout.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+import styles from "../styles/Layout.module.css";
+
+const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Payroll | Unfold 2022 submission</title>
+        <meta
+          name="description"
+          content="This project is a submission to Unfold 2022 hackathon hosted by Devfolio and CoinDCX"
+        />
+      </Head>
+      <header className={styles.header}>
+        <ConnectButton />
+      </header>
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
